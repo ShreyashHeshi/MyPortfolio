d5f396b26488e9229945ecee232a101042401d3f
thehesishreyash <shreyashesi3@gmail.com>
2024-12-21 00:05:48 +0530

    my portfolio

---
+++ my_portfolio/script.js
@@ -0,0 +1,6 @@
+let dayNight = document.querySelector(".dayNight");
+let banner = document.querySelector(".banner");
+
+dayNight.addEventListener("click",()=>{
+    banner.classList.toggle("night");
+})
